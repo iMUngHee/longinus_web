71f09a1ea9036f4a396ace3407beda3c1449b71e
Hee <unghee66@gmail.com>
2021-12-11 15:44:14 +0900

    Sibal

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router";
+import { ThemeProvider } from "styled-components";
+
+import GlobalStyles from "./styles/Global";
+import GlobalFonts from "./styles/Font";
+//eslint-disable-next-line
+import { lightTheme, darkTheme } from "./styles/Theme";
+
+import DashBoard from "./pages/DashBoard";
+
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <ThemeProvider theme={lightTheme}>
+      <GlobalStyles />
+      <GlobalFonts />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<DashBoard />} />
+          <Route path="/home" element={<DashBoard />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
