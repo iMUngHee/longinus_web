ec6aa76817f0d2167b483664f06444f84380c511
Hee <unghee66@gmail.com>
2021-12-20 20:49:41 +0900

    Constructing post layout_TextDeisign

--- src/styles/Theme.js
+++ src/styles/Theme.js
@@ -14,15 +14,17 @@ const globalTheme = {
 export const lightTheme = {
   primary: '#fff',
   secondary: '#f8f8f8',
+  opposite: '#302c40',
   textColor: '#585280',
   header: '#585280',
   headerNumber: '#fff',
-  activeMenu: '#585280',
+  activeMenu: '#582580',
   ...globalTheme
 }
 export const darkTheme = {
   primary: '#302c40',
   secondary: '#2c2839',
+  opposite: '#fff',
   textColor: '#fff',
   header: '#585280',
   headerNumber: '#fff',
